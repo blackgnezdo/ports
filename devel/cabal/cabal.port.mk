7f838fbbecbef42612c9932524249d717019c0c1
Greg Steuck <greg@nest.cx>
2020-09-13 17:04:03 -0700

    Add cabal.port.mk to more easily build haskell ports

---
+++ devel/cabal/cabal.port.mk
@@ -0,0 +1,138 @@
+# $OpenBSD$
+
+# Module for building Haskell programs with cabal-install.
+# Inspired by FreeBSD cabal.mk by Gleb Popov.
+
+# Useful variables:
+#
+#  MODCABAL_BUILD_ARGS - passed to cabal v2-build, e.g. make MODCABAL_BUILD_ARGS=-v
+#   is a nice debugging aid.
+#  MODCABAL_FLAGS - passed to --flags= of cabal v2-build. Seemingly superflous given
+#   MODCABAL_BUILD_ARGS, but it is useful to keep this value separate as it
+#   is used to generate the build plan where it will be available without parsing.
+
+ONLY_FOR_ARCHS =	i386 amd64
+
+BUILD_DEPENDS +=	devel/cabal \
+			lang/ghc
+
+MASTER_SITES0 =		https://hackage.haskell.org/package/
+
+DIST_SUBDIR ?= 		hackage
+
+# The .cabal files are explicitly copied over the ones extracted from
+# archives by the normal extraction rules.
+EXTRACT_CASES += *.cabal) ;;
+
+MODCABAL_HACKAGE_NAME =		${DISTNAME:C,-[0-9.]*$,,}
+MODCABAL_HACKAGE_VERSION =	${DISTNAME:C,.*-([0-9.]*)$,\1,}
+HOMEPAGE ?=			${MASTER_SITES0}${MODCABAL_HACKAGE_NAME}
+MASTER_SITES =			${MASTER_SITES0}${DISTNAME}/
+DISTFILES ?=			${DISTNAME}.tar.gz
+SUBST_VARS +=			DISTNAME MODCABAL_HACKAGE_NAME \
+				MODCABAL_HACKAGE_VERSION PKGNAME
+
+# Oftentime our port name and the executable name coincide.
+MODCABAL_EXECUTABLES ?=		${PKGSTEM}
+
+MODCABAL_post-extract = \
+	mkdir -p ${WRKDIR}/.cabal \
+	&& touch ${WRKDIR}/.cabal/config
+
+.if defined(MODCABAL_REVISION)
+DISTFILES += ${DISTNAME}_${MODCABAL_REVISION}{revision/${MODCABAL_REVISION}}.cabal
+MODCABAL_post-extract += \
+	&& cp ${FULLDISTDIR}/${DISTNAME}_${MODCABAL_REVISION}.cabal \
+		${WRKSRC}/${MODCABAL_HACKAGE_NAME}.cabal
+.endif
+
+.if defined(MODCABAL_MANIFEST)
+
+.for _package _version _revision in ${MODCABAL_MANIFEST}
+
+DISTFILES += {${_package}-${_version}/}${_package}-${_version}.tar.gz:0
+
+.if ${_revision} > 0
+DISTFILES += ${_package}-${_version}_${_revision}{${_package}-${_version}/revision/${_revision}}.cabal:0
+MODCABAL_post-extract += \
+	&& cp ${FULLDISTDIR}/${_package}-${_version}_${_revision}.cabal \
+		${WRKDIR}/${_package}-${_version}/${_package}.cabal
+.endif
+
+# References all the locally available dependencies.  Ideally these
+# should be command line options, tracking issue:
+# https://github.com/haskell/cabal/issues/3585
+MODCABAL_post-extract += \
+	&& echo "packages: ${WRKDIR}/${_package}-${_version}/${_package}.cabal" >> ${WRKSRC}/cabal.project.local
+.endfor
+
+.endif  # defined(MODCABAL_MANIFEST)
+
+# Automatically copies the cabal.project file if any.  TODO(gnezdo):
+# should this do any make variable substitution (like LOCALBASE)?
+# If not, maybe use --project_file and skip cp?
+MODCABAL_post-extract += \
+	&& (test -f ${FILESDIR}/cabal.project \
+	    && cp -v ${FILESDIR}/cabal.project ${WRKSRC}; true)
+
+MODCABAL_CABAL = ${SETENV} ${MAKE_ENV} HOME=${WRKDIR} ${LOCALBASE}/bin/cabal
+
+MODCABAL_BUILD_TARGET =	\
+	cd ${WRKBUILD} \
+	&& ${MODCABAL_CABAL} v2-build --offline --disable-benchmarks --disable-tests \
+		-j${MAKE_JOBS} \
+		--flags="${MODCABAL_FLAGS}" ${MODCABAL_BUILD_ARGS} \
+		${MODCABAL_EXECUTABLES:C/^/exe:&/}
+
+_MODCABAL_INSTALL_TARGET = true
+
+.if defined(MODCABAL_DATA_DIRS)
+_MODCABAL_LIBEXEC = libexec/cabal
+_MODCABAL_INSTALL_TARGET += \
+	&& mkdir -p ${PREFIX}/${_MODCABAL_LIBEXEC}
+
+.for _data in ${MODCABAL_DATA_DIRS}
+_MODCABAL_INSTALL_TARGET +=  \
+	&& ${INSTALL_DATA_DIR} ${WRKSRC}/${_data} ${PREFIX}/share/${DISTNAME} \
+	&& cd ${WRKSRC}/${_data} && umask 022 && pax -rw . ${PREFIX}/share/${DISTNAME}
+.endfor
+.endif
+
+.for _exe in ${MODCABAL_EXECUTABLES}
+
+# Exports the paths to executables for testing.
+MODCABAL_BUILT_EXECUTABLE_${_exe} = $$(find ${WRKSRC}/dist-newstyle -name ${_exe} -type f -perm -a+x)
+
+.if defined(MODCABAL_DATA_DIRS)
+
+.for _data in ${MODCABAL_DATA_DIRS}
+_MODCABAL_EXPORTS += \
+	&& echo 'export ${_exe}_datadir=${LOCALBASE}/share/${DISTNAME}' >> ${PREFIX}/bin/${_exe}
+.endfor
+
+_MODCABAL_INSTALL_TARGET += \
+	&& ${INSTALL_PROGRAM} \
+		${MODCABAL_BUILT_EXECUTABLE_${_exe}} \
+		${PREFIX}/${_MODCABAL_LIBEXEC}/${_exe} \
+	&& echo '\#!/bin/sh' > ${PREFIX}/bin/${_exe} \
+	${_MODCABAL_EXPORTS} \
+	&& echo 'exec ${LOCALBASE}/${_MODCABAL_LIBEXEC}/${_exe} "$$@"' >> ${PREFIX}/bin/${_exe} \
+	&& chmod +x ${STAGEDIR}${PREFIX}/bin/${_exe}
+.else
+# Skips the launcher script indirection when MODCABAL_DATA_DIRS is empty.
+# TODO(gnezdo): evaluate if this case is common enough to be warranted.
+_MODCABAL_INSTALL_TARGET += \
+	&& ${INSTALL_PROGRAM} ${MODCABAL_BUILT_EXECUTABLE_${_exe}} ${PREFIX}/bin
+.endif
+
+.endfor
+
+.if !target(do-build)
+do-build:
+	@${MODCABAL_BUILD_TARGET}
+.endif
+
+.if !target(do-install)
+do-install:
+	@${_MODCABAL_INSTALL_TARGET}
+.endif
